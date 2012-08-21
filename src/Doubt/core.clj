d8b7c6d9eb2536cc1489680f48c612c25fc1f612
kurmis <mindaugas.kurmauskas@gmail.com>
2012-08-22 02:20:42 +0300

    fix for jdbc lib

--- src/Doubt/core.clj
+++ src/Doubt/core.clj
@@ -1,7 +1,7 @@
 (ns Doubt.core)
 ;(use 'clojure.java.jdbc)
-(recquire '[clojure.java.jdbc :as sql])
-(let (db
+(require '[clojure.java.jdbc :as sql])
+;(let (db
 (defn -main
   "I don't do a whole lot."
   [& args]
